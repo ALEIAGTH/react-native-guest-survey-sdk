13b5652146e2e4a3fb6b689e4d4798ba10441bed
Xinrui GUO <xinrui.guo@it4pme.fr>
2022-02-08 10:35:04 +0100

    Update config and language method

--- android/src/main/java/com/reactnativeguestsurveysdk/GuestSurveySdkModule.java
+++ android/src/main/java/com/reactnativeguestsurveysdk/GuestSurveySdkModule.java
@@ -35,69 +35,69 @@ import fr.it4pme.locatme.Locatme;
 
 @ReactModule(name = GuestSurveySdkModule.NAME)
 public class GuestSurveySdkModule extends ReactContextBaseJavaModule {
-    public static final String NAME = "GuestSurveySdk";
-    private static ReactApplicationContext mContext;
-    private static String serverId;
-    private static ProgramDownloadResult result;
-    private static String appKey = "";
-    private static String appName = "";
-    private static String vendorClientId = "";
-    private static String vendorSecretKey = "";
-    private static String userID = "";
+  public static final String NAME = "GuestSurveySdk";
+  private static ReactApplicationContext mContext;
+  private static String serverId;
+  private static ProgramDownloadResult result;
+  private static String appKey = "";
+  private static String appName = "";
+  private static String vendorClientId = "";
+  private static String vendorSecretKey = "";
+  private static String userID = "";
 
-    public GuestSurveySdkModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        mContext = reactContext;
-    }
+  public GuestSurveySdkModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    mContext = reactContext;
+  }
 
-    @Override
-    @NonNull
-    public String getName() {
-        return NAME;
-    }
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
 
-    @ReactMethod
-    public static void configure(Callback callback) {
-        Locatme.configure(mContext, new Locatme.configueCallback() {
-          @Override
-          public void onResponse(String s, JSONObject jsonObject) {
-                new ConfirmitSDK.Setup((android.app.Application) mContext.getApplicationContext()).configure();
-                System.out.println("LocatmeJson -> "+jsonObject);
-                try {
-                  vendorClientId = jsonObject.getString("vendorClientId");
-                  vendorSecretKey = jsonObject.getString("vendorSecretKey");
-                  appKey = jsonObject.getString("appKey");
-                  appName = jsonObject.getString("appName");
-                  SaveSharedPreference.setAppKey(mContext,appKey);
-                  SaveSharedPreference.setAppName(mContext,appName);
-                } catch (JSONException e) {
-                  e.printStackTrace();
-                }
+  @ReactMethod
+  public static void configure(String secretKey, String language, boolean enableDebugging, Callback callback) {
+    Locatme.configure(mContext, secretKey, language, enableDebugging, new Locatme.configueCallback() {
+      @Override
+      public void onResponse(String s, JSONObject jsonObject) {
+        new ConfirmitSDK.Setup((android.app.Application) mContext.getApplicationContext()).configure();
+        System.out.println("LocatmeJson -> "+jsonObject);
+        try {
+          vendorClientId = jsonObject.getString("vendorClientId");
+          vendorSecretKey = jsonObject.getString("vendorSecretKey");
+          appKey = jsonObject.getString("appKey");
+          appName = jsonObject.getString("appName");
+          SaveSharedPreference.setAppKey(mContext,appKey);
+          SaveSharedPreference.setAppName(mContext,appName);
+        } catch (JSONException e) {
+          e.printStackTrace();
+        }
 
-                ConfirmitServer.INSTANCE.configureUS(vendorClientId, vendorSecretKey);
-                serverId = ConfirmitServer.INSTANCE.getUS().getServerId();
-                result = TriggerSDK.INSTANCE.download(serverId, mContext.getString(R.string.program_key));
+        ConfirmitServer.INSTANCE.configureUS(vendorClientId, vendorSecretKey);
+        serverId = ConfirmitServer.INSTANCE.getUS().getServerId();
+        result = TriggerSDK.INSTANCE.download(serverId, mContext.getString(R.string.program_key));
 
-                if(result.getResult()) {
-                  // Your Digital Feedback program is started!
-                  System.out.println("Your Digital Feedback program is started!");
-                  callback.invoke("ok");
-                } else {
-                  // Digital Feedback program download failed
-                  // Tip: a possible reason is that your program contains scripting errors
-                  System.out.println("Digital Feedback program download failed");
-                  callback.invoke("failed");
-                }
-          }
-       });
-    }
+        if(result.getResult()) {
+          // Your Digital Feedback program is started!
+          callback.invoke("ok");
+        } else {
+          // Digital Feedback program download failed
+          // Tip: a possible reason is that your program contains scripting errors
+          callback.invoke("failed");
+        }
+        ConfirmitSDK.INSTANCE.enableLog(enableDebugging);
+      }
+    });
+  }
 
-    @ReactMethod
-    public static void startCollection(String userId) {
-      checkPermission(mContext.getCurrentActivity());
-      SaveSharedPreference.setUserID(mContext,userId);
-      Locatme.startCollection(mContext,userId);
-    }
+  @ReactMethod
+  public static void startCollection(String userId) {
+    userID = userId;
+    checkPermission(mContext.getCurrentActivity());
+    SaveSharedPreference.setUserID(mContext,userId);
+    Locatme.startCollection(mContext,userId);
+  }
 
   public static boolean checkPermission(Activity activity) {
     List<String> permissionsToRequest = new ArrayList<>();
@@ -123,68 +123,53 @@ public class GuestSurveySdkModule extends ReactContextBaseJavaModule {
     }
   }
 
-    @ReactMethod
-    public static void stopCollection() {
-      Locatme.stopCollection(mContext);
-      SaveSharedPreference.clearData(mContext);
-    }
+  @ReactMethod
+  public static void stopCollection() {
+    Locatme.stopCollection(mContext);
+    SaveSharedPreference.clearData(mContext);
+  }
 
-    @ReactMethod
-    public static void getUserSurveys( Callback callback) {
-
-      Locatme.configure(mContext, new Locatme.configueCallback() {
-          @Override
-          public void onResponse(String s, JSONObject jsonObject) {
-            System.out.println("getUserSurveys : "+s);
-            userID = SaveSharedPreference.getUserID(mContext);
-            String key = SaveSharedPreference.getAppKey(mContext);
-            String mAppName = SaveSharedPreference.getAppName(mContext);
-
-            System.out.println("getUserSurveys userID: "+userID);
-            System.out.println("getUserSurveys AppName: "+SaveSharedPreference.getAppName(mContext));
-              if ( userID!= ""){
-                Locatme.getUserSurveys(userID, key, mAppName, new Locatme.Callback() {
-                  @Override
-                  public void onResponse(String s, JSONArray jsonArray) {
-                    System.out.println("getUserSurveys : "+jsonArray);
-                    callback.invoke(jsonArray.toString());
-                  }
-                });
-            }
+  @ReactMethod
+  public static void getUserSurveys(Callback callback) {
+    if (userID!= ""){
+      Locatme.getUserSurveys(userID, appKey, appName, new Locatme.Callback() {
+        @Override
+        public void onResponse(String s, JSONArray jsonArray) {
+          System.out.println("getUserSurveys : "+jsonArray);
+          callback.invoke(jsonArray.toString());
         }
       });
     }
+  }
 
-    @ReactMethod
-    public static void sendLog() {
-      Locatme.sendLog(mContext);
+  @ReactMethod
+  public static void sendLog() {
+    Locatme.sendLog(mContext);
+  }
+
+  @ReactMethod
+  public static void addPushNotificationToken(String token) {
+    Locatme.addPushNotificationToken(mContext, token);
+  }
+
+  @ReactMethod
+  public static void triggerSurvey(String programKey, String eventName){
+    if(result.getResult()) {
+      new Handler(Looper.getMainLooper()).post(new Runnable() {
+        @Override
+        public void run() {
+          Intent intent = new Intent(mContext.getCurrentActivity(), SurveyFragment.class);
+          intent.putExtra("programKey",programKey);
+          intent.putExtra("eventName",eventName);
+          intent.putExtra("email",SaveSharedPreference.getUserID(mContext));
+          intent.putExtra("appName",SaveSharedPreference.getAppName(mContext));
+          intent.putExtra("appKey",SaveSharedPreference.getAppKey(mContext));
+          intent.addFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+          mContext.getCurrentActivity().startActivityForResult(intent,123);
+        }
+      });
+    } else {
+      System.out.println("Digital Feedback program download failed");
     }
-
-    @ReactMethod
-    public static void addPushNotificationToken(String token) {
-      Locatme.addPushNotificationToken(mContext, token);
-    }
-
-    @ReactMethod
-    public static void triggerSurvey(String programKey, String eventName){
-      if(result.getResult()) {
-        System.out.println("Your Digital Feedback program is started!");
-
-        new Handler(Looper.getMainLooper()).post(new Runnable() {
-          @Override
-          public void run() {
-            Intent intent = new Intent(mContext.getCurrentActivity(), SurveyFragment.class);
-            intent.putExtra("programKey",programKey);
-            intent.putExtra("eventName",eventName);
-            intent.putExtra("email",SaveSharedPreference.getUserID(mContext));
-            intent.putExtra("appName",SaveSharedPreference.getAppName(mContext));
-            intent.putExtra("appKey",SaveSharedPreference.getAppKey(mContext));
-            intent.addFlags(intent.FLAG_ACTIVITY_NEW_TASK);
-            mContext.getCurrentActivity().startActivityForResult(intent,123);
-          }
-        });
-      } else {
-        System.out.println("Digital Feedback program download failed");
-      }
-    }
+  }
 }
